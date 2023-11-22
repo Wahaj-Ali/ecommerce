b38ae06893a4602931288399f46578667f44ceaf
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-11-23 00:55:22 +0500

    Fetch data from sanity for the hero banner component.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { Product, HeroBanner, Footer, Navbar, FooterBanner, Cart, Layout } from '@/components';
+import { client } from '@/lib/client';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-    <HeroBanner />
-
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+    {console.log(bannerData)}
     <div className='products-heading'>
       <h2>Best Selling Products</h2>
       <p>Shoes of your Choice, Comfortable and Elegant Looking!</p>
     </div>
 
     <div className='products-container'>
-      {['Product1', 'Product2'].map((product) => 
-        product
+      {products?.map((product) => 
+        product.name
       )}
     </div>
 
     <Footer />
     </>
   )
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]';
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
